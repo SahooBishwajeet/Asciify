dcf30f88f5dd924342766c586f0fc1d46cc30f82
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2023-10-15 00:30:13 +0530

    Added Mirror Button, which will give the user an option to select the mirroring property of the resultant feed i.e. Mirrored Feed or Non-Mirrored Feed

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
-const density = "Ñ@#W$9876543210?!abc;:+=-,._                    ";         // Add Sapces to control the Threshold
-// const density = '       .:-i|=+%O#@'
-// const density = '        .:░▒▓█';
+let density = "Ñ@#W$9876543210?!abc;:+=-,._ ";         // Add Sapces to control the Threshold
+// let density = '       .:-i|=+%O#@'
+// let density = '        .:░▒▓█';
 
 let video;
 let asciiDiv;
@@ -12,14 +12,30 @@ function setup() {
   asciiDiv = createDiv();
 }
 
+let isMirrored = false;
+function choose(mirror, i, j, width) {
+  let a = ((width - i - 1) + j * width) * 4;
+  let b = (i + j * width) * 4;
+  if (mirror == true)
+    return a;
+  else
+    return b;
+}
+
+const mirrorButton = document.getElementById("mirrorButton");
+mirrorButton.addEventListener("click", function () {
+  isMirrored = !isMirrored;
+});
+
 function draw() {
   video.loadPixels();
   let asciiImage = "";
 
   for (let j = 0; j < video.height; j++) {
     for (let i = 0; i < video.width; i++) {
-      const pixelIndex = ((video.width - i - 1) + j * video.width) * 4;      // Mirror Feed
+      // const pixelIndex = ((video.width - i - 1) + j * video.width) * 4;      // Mirror Feed
       // const pixelIndex = (i + j * video.width) * 4;                           // Real Feed
+      const pixelIndex = choose(isMirrored, i, j, video.width);
       const r = video.pixels[pixelIndex + 0];
       const g = video.pixels[pixelIndex + 1];
       const b = video.pixels[pixelIndex + 2];
